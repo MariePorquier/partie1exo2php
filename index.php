0dada4297c5e78d6da7380efcaf9e60660fc907f
MariePorquier <marieporquier@free.fr>
2019-12-02 16:41:18 +0100

    PHPpartie1exo2

--- index.php
+++ index.php
@@ -1,22 +1,42 @@
-<!DOCTYPE html>
 <?php
 //déclaration de la variable lastname
-$lastname= 'Nom de famille';
+$lastname = 'Nom de famille';
 //déclaration de la variable firstname
-$firstname= 'Prénom';
+$firstname = 'Prénom';
 //déclaration de la variable age
-$age= 30;
+$age = 30;
  ?>
+<!DOCTYPE html>
 <html lang=fr dir="ltr">
   <head>
     <meta charset="utf-8" />
     <title>PHPpartie1exo2</title>
   </head>
   <body>
-  <p>
-    <?php echo $lastname; ?>
-    <?php echo $firstname; ?>
-    <?php echo $age; ?>
-  </p>
+   <p>
+      <!--Affichage dans Html -->
+    <?php
+    echo $lastname;
+    echo $firstname;
+    echo $age;
+    ?>
+   </p>
+   <p>
+    <?php
+     echo $lastname . ' ' . $firstname . ' ' . $age;
+    ?>
+   </p>
+   <p>
+    <?= $lastname . ' ' . $firstname . ' ' . $age; ?>
+   </p>
+   <p>
+    Nom : <?= $lastname; ?>
+   </p>
+   <p>
+    Prénom : <?= $firstname; ?>
+   </p>
+   <p>
+    Age : <?= $age; ?> ans
+   </p>
   </body>
 </html>
