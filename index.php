de3119c1f27ae421bcc30f241e359adb63d1765b
MariePorquier <marieporquier@free.fr>
2019-12-02 15:13:59 +0100

    partie1exo2php

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<?php
+//déclaration de la variable lastname
+$lastname= 'Nom de famille';
+//déclaration de la variable firstname
+$firstname= 'Prénom';
+//déclaration de la variable age
+$age= 30;
+ ?>
+<html lang=fr dir="ltr">
+  <head>
+    <meta charset="utf-8" />
+    <title>PHPpartie1exo2</title>
+  </head>
+  <body>
+  <p>
+    <?php echo $lastname; ?>
+    <?php echo $firstname; ?>
+    <?php echo $age; ?>
+  </p>
+  </body>
+</html>
